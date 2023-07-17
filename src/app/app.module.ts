ca44b514ef377c1c0855a7e04810132e927c5a7c
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-07-17 16:13:48 -0300

    Criado um componet header para o projeto, e realizado suas estilizações.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TabelaDadosComponent } from './tabela-dados/tabela-dados.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabelaDadosComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
