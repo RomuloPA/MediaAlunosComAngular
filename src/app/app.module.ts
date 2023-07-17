615cd325068627c5ef30ced1ec497d918acb479a
Jéssica Pamplona De Almeida <romulopamplona958@gmail.com>
2023-07-17 17:21:13 -0300

    Criado o component footer e suas esetilizações.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { TabelaDadosComponent } from './tabela-dados/tabela-dados.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabelaDadosComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
